10eef940aff996fdc26c2bbc75cb83a4e6254b58
Michael Zwickler <zwickler.michael@googlemail.com>
2022-01-03 11:08:26 +0100

    checkout class finished

--- TDD_Simple_Checkout/lib/checkout.rb
+++ TDD_Simple_Checkout/lib/checkout.rb
@@ -1,2 +1,19 @@
 class Checkout
+
+  def initialize()
+    @basket = []
+  end
+
+  def scan(item)
+    @basket.push(item)
+  end
+
+  def show_total()
+    sum = 0
+    @basket.each do |item|
+      sum += item.price
+    end
+    
+    return "£#{sprintf("%#.2f", sum)}"
+  end
 end
--- TDD_Simple_Checkout/spec/checkout_spec.rb
+++ TDD_Simple_Checkout/spec/checkout_spec.rb
@@ -1,4 +1,20 @@
 require 'checkout'
 
 RSpec.describe Checkout do
+
+  describe '#scan' do
+    it 'scans an item and adds to the basket' do 
+      item = Item.new('Banana', 5)
+      expect(subject.scan(item)).to eq([item])
+    end
+  end
+
+  describe '#show_total' do
+    it 'shows the sum of the shopping basket' do
+      subject.scan(Item.new('Banana', 5))
+      subject.scan(Item.new('Apple', 3.59))
+      expect(subject.show_total()).to eq ('£8.59')
+    end
+  end
+
 end
