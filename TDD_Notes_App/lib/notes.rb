627bf5897b15707a7506a28ea582dd33292ec50f
Michael Zwickler <zwickler.michael@googlemail.com>
2022-01-03 15:06:43 +0100

    #add_note implemented

--- TDD_Notes_App/lib/notes.rb
+++ TDD_Notes_App/lib/notes.rb
@@ -1,2 +1,12 @@
 class Notes
+
+  def initialize()
+    @notes = {}
+  end
+
+  def add_note(title, body)
+    @notes[title.to_sym] = body 
+    return @notes 
+  end
+
 end
--- TDD_Notes_App/spec/note_spec.rb
+++ TDD_Notes_App/spec/note_spec.rb
@@ -4,7 +4,17 @@ RSpec.describe Notes do
 
   describe '#add_note' do
     it 'it adds a note, and returns all notes currently stored' do
-      expect (subject.add_note('Testtitel', 'Testbody')).to eq({Testtitel: 'Body'})
+      subject.add_note('Titel1', 'Body1')
+      expect(subject.add_note('Titel2', 'Body2')).to eq({Titel1: 'Body1', Titel2: 'Body2'})
+    end
+  end
+
+  describe '#list_titles' do
+    it 'it returns a list of all titles stored' do
+      subject.add_note('Titel1', 'Body1')
+      subject.add_note('Titel2', 'Body2')
+      subject.add_note('Titel3', 'Body3')
+      expect(subject.list_titles).to eq([:Titel1, :Titel2, :Titel3])
     end
   end
 
